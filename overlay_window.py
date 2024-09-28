7afa54ecbb972d15722431df89824ba226c3d5d0
Tiqur <trevorbrage02@gmail.com>
2024-09-27 21:25:57 -0700

    Add basic shortcut labels

--- overlay_window.py
+++ overlay_window.py
@@ -12,6 +12,8 @@ class OverlayWindow(QMainWindow):
         self.setAttribute(Qt.WA_TranslucentBackground)
         self.setAttribute(Qt.WA_TransparentForMouseEvents)
 
+        self.setStyleSheet('font-size: 16px')
+
         self.setWindowState(Qt.WindowFullScreen)
         self.setWindowFlags(self.windowFlags() | Qt.X11BypassWindowManagerHint)
 
@@ -22,6 +24,24 @@ class OverlayWindow(QMainWindow):
     
         self.boxes = []
         self.update_signal.connect(self.update)
+        self.sequence = 1
+
+
+    def to_base_26(self, num):
+        """Convert a number to a base-26 representation using letters."""
+        result = []
+        while num > 0:
+            num -= 1  # Adjust for 0-indexing
+            result.append(chr(num % 26 + ord('a')))
+            num //= 26
+        return ''.join(reversed(result))
+
+    def get_next_letter(self):
+        """Return the next letter in the sequence and increment the counter."""
+        letter = self.to_base_26(self.sequence)  # Get the current letter
+        self.sequence += 1  # Increment to prepare for the next call
+        return letter
+
 
     def add_box(self, box):
         self.boxes.append(box)
@@ -45,12 +65,23 @@ class OverlayWindow(QMainWindow):
         painter.drawRect(self.rect().adjusted(2, 2, -2, -2))
 
         # Draw all boxes
+        # In the future, draw all at once using painter.drawRects(Rect[])
         pen = QPen(QColor(0, 255, 255))
         painter.setPen(pen)
         for box in self.boxes:
+            tl_point = box.topLeft()
+            x = tl_point.x()
+            y = tl_point.y()
+            pen = QPen(QColor(0, 0, 0))
+            painter.fillRect(x, y-15, 30, 15, QColor(0, 255, 255))
+            painter.setPen(QColor(0, 0, 0))
+            shortcut = self.get_next_letter()
+            painter.drawText(x, y-2, shortcut)
+            painter.setPen(QColor(0, 255, 255))
             painter.drawRect(box)
 
         self.clear_boxes()
+        self.sequence = 1
 
         # Show settings at top right
         max_settings_width = 105
